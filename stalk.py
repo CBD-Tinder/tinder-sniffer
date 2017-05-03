de9255002200656384227bf0f743de5c9da70cb2
David de los Santos Boix <ddlsb@hotmail.com>
2017-05-03 18:22:40 +0200

    Padre, lo de la tontuna

--- stalk.py
+++ stalk.py
@@ -5,7 +5,7 @@ from utils import get_data_to_update, get_bd_session
 from models import init_database, User
 
 # Logging in Tinder...
-session = pynder.Session(facebook_id, facebook_token)
+session = pynder.Session(facebook_id=facebook_id, facebook_token=facebook_token)
 
 # Checking the parameters... If one of them is outdated, we just update it
 new_params = get_data_to_update(session.profile, distance_filter, age_filter_max, age_filter_min)
@@ -13,13 +13,18 @@ new_params = get_data_to_update(session.profile, distance_filter, age_filter_max
 if len(new_params) != 0:
     session.update_profile(new_params)
 
-db_session = get_bd_session(db_url)
+#db_session = get_bd_session(db_url)
 
-print("aqui estamos hamijos")
 # If there isn't new people nearby, it just launches an exception, so....
 for user in session.nearby_users(limit=search_limit):
-    if db_session.query(User).filter(User.id == user.id).count() == 0:
-        new_user = User(id = user.id, age= user.age, gender = user.gender, name = user.name)
-        db_session.add(new_user)
+    #if db_session.query(User).filter(User.id == user.id).count() == 0:
+    print(user.id)
+    print(user.name)
+    print(user.age)
+    print(user.gender)
+    print(user.distance_km)
 
-db_session.commit()
+        #new_user = User(id = user.id, age= user.age, gender = user.gender, name = user.name)
+        #db_session.add(new_user)
+
+#db_session.commit()
